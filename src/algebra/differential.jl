9a8ece3290c8c0581d51ce37e2593eeb4567e3a8
sminez <innes.morrison@cocoon.life>
2016-12-11 08:52:19 +0000

    breaking del filters into functions

--- src/algebra/differential.jl
+++ src/algebra/differential.jl
@@ -51,9 +51,9 @@ del_i(vec::Vector) = ∇i(vec)
 D_mu(vec::Vector) = Dμ(vec)
 
 
-##############################################
-# .: Operations on differentiated vectors :. #
-##############################################
+###################################################
+# .: Grouping terms for display of derivatives :. #
+###################################################
 """__by_α__ :: Vector{symbolic_ξα} -> Iterators.GroupBy
 
 Group derivative terms according to their unit element.
@@ -68,6 +68,147 @@ function by_α(vec::Vector{symbolic_ξα}, vector_groups=false)
 end
 
 
+function replace_grad(components, group_name, groups_ξs)
+    output = Vector{symbolic_ξα}()
+
+    function grad_check(j::symbolic_ξα, comp::symbolic_ξα, wrt::String)
+        comp_match = j.xi.unit == comp.xi.unit
+        ∂_match = j.xi.partials[level].index == wrt
+        return comp_match && ∂_match
+    end
+
+    for comp in components
+        grad_elements = []
+        for wrt in ["1","2","3"]
+            elem = filter(j->grad_check(j,comp,wrt), components)
+            elem != [] && push!(grad_elements, elem[1])
+        end
+
+        if length(grad_elements) == 3
+            first_α = grad_elements[1].alpha
+            α_group = get(α_TO_GROUP, first_α.index, first_α.index)
+            grad_component = symbolic_ξ("∇Ξ$group_name")
+            grad_α = α(Symbol(α_group), first_α.sign)
+            push!(output, symbolic_ξα(grad_α, grad_component))
+            filter!(x -> !(x in grad_elements), components)
+        end
+        return (components, output)
+    end
+end
+
+
+function replace_div(components, group_name, groups_ξs)
+    output = Vector{symbolic_ξα}()
+
+    function div_check(j::symbolic_ξα)
+        ∂_index = parse(j.xi.partials[level].index)
+        return ∂_index == 0 ? false : group_ξs[∂_index] == j.xi.unit
+    end
+
+    div_elements = filter(div_check, components)
+    if div_elements != []
+        first_α = div_elements[1].alpha
+
+        if all([d.alpha == first_α for d in div_elements])
+            # XXX:: This is either an α index or the name of a Ξ group
+            α_group = get(α_TO_GROUP, first_α.index, first_α.index)
+            div_component = symbolic_ξ("∇•Ξ$group_name")
+            div_α = α(Symbol(α_group), first_α.sign)
+            push!(output, symbolic_ξα(div_α, div_component))
+            filter!(x -> !(x in div_elements), components)
+        end
+    end
+    return (components, output)
+end
+
+
+function replace_curl(components, group_name, group_ξs)
+    output = Vector{symbolic_ξα}()
+
+    function curl_check(j::symbolic_ξα, term::Symbol, wrt::α)
+        diff = j.xi.partials[level]
+        return (j.xi.val == term) && (diff == wrt)
+    end
+
+    curl_elements = Vector{Tuple{symbolic_ξα,Symbol}}()
+    curl_element_missing = false
+
+    for (i, index) in enumerate(group_ξs)
+        # curl is [∂cw(ξacw) - ∂acw(ξcw)] ∀ indices in group_ξs
+        # NOTE:: This is [pos, neg] below until I work out some better
+        #        names for everything!
+        ∂_cw = α(string(CW[i]))
+        ∂_acw = α(string(ACW[i]))
+        ξ_cw = Symbol("ξ" * group_ξs[CW[i]])
+        ξ_acw = Symbol("ξ" * group_ξs[ACW[i]])
+        pos = filter(j->curl_check(j, ξ_acw, ∂_cw), components)
+        neg = filter(j->curl_check(j, ξ_cw, ∂_acw), components)
+
+        if pos != [] && neg != []
+            # extract the filtered elements from the arrays
+            pos, neg = pos[1], neg[1]
+
+            if pos.alpha.sign == 1 && neg.alpha.sign == -1
+                append!(curl_elements, [(pos, :+), (neg, :+)])
+            elseif pos.alpha.sign == -1 && neg.alpha.sign == 1
+                append!(curl_elements, [(pos, :-), (neg, :-)])
+            else
+                curl_element_missing = true
+            end
+        else
+            curl_element_missing = true
+        end
+        curl_element_missing && break
+    end
+
+    if length(curl_elements) == 6
+        sign = 0
+        if all(comp[2] == :+ for comp in curl_elements)
+            sign = 1
+        elseif all(comp[2] == :- for comp in curl_elements)
+            sign = -1
+        end
+
+        curl_component = symbolic_ξ("∇xΞ$group_name")
+        first_α = curl_elements[1][1].alpha
+        α_group = get(α_TO_GROUP, first_α.index, first_α.index)
+        curl_α = α(Symbol(α_group), sign)
+        push!(output, symbolic_ξα(curl_α, curl_component))
+        filter!(x -> !(x in [e[1] for e in curl_elements]), components)
+    end
+    return (components, output)
+end
+
+
+function replace_group_partials(components, group_name, group_ξs)
+    output = Vector{symbolic_ξα}()
+
+    function dmu_check(j::symbolic_ξα, comp::String, wrt::String)
+        j.xi.unit == comp && j.xi.partials[level].index == wrt
+    end
+
+    for wrt in ["0","1","2","3"]
+        xi_elements = []
+        for comp in group_ξs
+            elem = filter(j->dmu_check(j,comp,wrt), components)
+            if elem != []
+                push!(xi_elements, elem[1])
+            end
+        end
+
+        if length(xi_elements) == 3
+            first_α = xi_elements[1].alpha
+            α_group = get(α_TO_GROUP, first_α.index, first_α.index)
+            xi_component = symbolic_ξ("∂$(wrt)Ξ$group_name")
+            xi_α = α(Symbol(α_group), first_α.sign)
+            push!(output, symbolic_ξα(xi_α, xi_component))
+            filter!(x -> !(x in xi_elements), components)
+        end
+    end
+    return (components, output)
+end
+
+
 """__by_∇__ :: Vector{symbolic_ξα} -> Vector{symbolic_ξα}
 
 Attempt to identify vector derivatives in each group
@@ -76,136 +217,21 @@ function by_∇(vec::Vector{symbolic_ξα}, level=1)
     output = Vector{symbolic_ξα}()
 
     for components in by_α(vec, true)
-        for (group_name, group_ξs) in ξ_GROUPS
-            ####################################################################
-            # Check for ∇group_name
-            function grad_check(j::symbolic_ξα, comp::symbolic_ξα, wrt::String)
-                comp_match = j.xi.unit == comp.xi.unit
-                ∂_match = j.xi.partials[level].index == wrt
-                return all([comp_match, ∂_match])
-            end
+        for (group, ξs) in ξ_GROUPS
+            components, grads = replace_grad(components, group, ξs)
+            append!(output, grads)
 
-            for comp in components
-                grad_elements = []
-                for wrt in ["1","2","3"]
-                    elem = filter(j->grad_check(j,comp,wrt), components)
-                    if elem != []
-                        push!(grad_elements, elem[1])
-                    end
-                end
+            components, divs = replace_div(components, group, ξs)
+            append!(output, divs)
 
-                if length(grad_elements) == 3
-                    first_α = grad_elements[1].alpha
-                    α_group = get(α_TO_GROUP, first_α.index, first_α.index)
-                    grad_component = symbolic_ξ("∇Ξ$group_name")
-                    grad_α = α(Symbol(α_group), first_α.sign)
-                    push!(output, symbolic_ξα(grad_α, grad_component))
-                    filter!(x -> !(x in grad_elements), components)
-                end
-            end
+            components, curls = replace_curl(components, group, ξs)
+            append!(output, curls)
 
-            ####################################################################
-            # Check for ∇•group_name
-            function div_check(j::symbolic_ξα)
-                ∂_index = parse(j.xi.partials[level].index)
-                return ∂_index == 0 ? false : group_ξs[∂_index] == j.xi.unit
-            end
-
-            div_elements = filter(div_check, components)
-            if div_elements != []
-                first_α = div_elements[1].alpha
-
-                if all([d.alpha == first_α for d in div_elements])
-                    # XXX:: This is either an α index or the name of a Ξ group
-                    α_group = get(α_TO_GROUP, first_α.index, first_α.index)
-                    div_component = symbolic_ξ("∇•Ξ$group_name")
-                    div_α = α(Symbol(α_group), first_α.sign)
-                    push!(output, symbolic_ξα(div_α, div_component))
-                    filter!(x -> !(x in div_elements), components)
-                end
-            end
-
-            ####################################################################
-            # Check for ∇xgroup_name
-            function curl_check(j::symbolic_ξα, term::Symbol, wrt::α)
-                diff = j.xi.partials[level]
-                return (j.xi.val == term) && (diff == wrt)
-            end
-
-            curl_elements = Vector{Tuple{symbolic_ξα,Symbol}}()
-            curl_element_missing = false
-
-            for (i, index) in enumerate(group_ξs)
-                # curl is [∂cw(ξacw) - ∂acw(ξcw)] ∀ indices in group_ξs
-                # NOTE:: This is [pos, neg] below until I work out some better
-                #        names for everything!
-                ∂_cw = α(string(CW[i]))
-                ∂_acw = α(string(ACW[i]))
-                ξ_cw = Symbol("ξ" * group_ξs[CW[i]])
-                ξ_acw = Symbol("ξ" * group_ξs[ACW[i]])
-                pos = filter(j->curl_check(j, ξ_acw, ∂_cw), components)
-                neg = filter(j->curl_check(j, ξ_cw, ∂_acw), components)
-
-                if pos != [] && neg != []
-                    # extract the filtered elements from the arrays
-                    pos, neg = pos[1], neg[1]
-
-                    if pos.alpha.sign == 1 && neg.alpha.sign == -1
-                        append!(curl_elements, [(pos, :+), (neg, :+)])
-                    elseif pos.alpha.sign == -1 && neg.alpha.sign == 1
-                        append!(curl_elements, [(pos, :-), (neg, :-)])
-                    else
-                        curl_element_missing = true
-                    end
-                else
-                    curl_element_missing = true
-                end
-                curl_element_missing && break
-            end
-
-            if length(curl_elements) == 6
-                sign = 0
-                if all(comp[2] == :+ for comp in curl_elements)
-                    sign = 1
-                elseif all(comp[2] == :- for comp in curl_elements)
-                    sign = -1
-                end
-
-                curl_component = symbolic_ξ("∇xΞ$group_name")
-                first_α = curl_elements[1][1].alpha
-                α_group = get(α_TO_GROUP, first_α.index, first_α.index)
-                curl_α = α(Symbol(α_group), sign)
-                push!(output, symbolic_ξα(curl_α, curl_component))
-                filter!(x -> !(x in [e[1] for e in curl_elements]), components)
-            end
-
-            ####################################################################
-            # Check for ∂μ group_name
-            function dmu_check(j::symbolic_ξα, comp::String, wrt::String)
-                j.xi.unit == comp && j.xi.partials[level].index == wrt
-            end
-
-            for wrt in ["0","1","2","3"]
-                xi_elements = []
-                for comp in group_ξs
-                    elem = filter(j->dmu_check(j,comp,wrt), components)
-                    if elem != []
-                        push!(xi_elements, elem[1])
-                    end
-                end
-
-                if length(xi_elements) == 3
-                    first_α = xi_elements[1].alpha
-                    α_group = get(α_TO_GROUP, first_α.index, first_α.index)
-                    xi_component = symbolic_ξ("∂$(wrt)Ξ$group_name")
-                    xi_α = α(Symbol(α_group), first_α.sign)
-                    push!(output, symbolic_ξα(xi_α, xi_component))
-                    filter!(x -> !(x in xi_elements), components)
-                end
-            end
-
+            components, group_∂s = replace_group_partials(components, group, ξs)
+            append!(output, group_∂s)
         end
-        # XXX:: add remaining elements under the correct grouped α value
+
+        # Add remaining elements under the correct grouped α value
         for component in components
             i = component.alpha.index
             component.alpha.index = get(α_TO_GROUP, i, i)
