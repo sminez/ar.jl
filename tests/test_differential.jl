031b3e652d98d705ec2a58fc752e6e87bcde2492
sminez <innes.morrison@cocoon.life>
2016-12-15 08:29:39 +0000

    adding more visualisations and notes on comparison of differentials

---
+++ tests/test_differential.jl
@@ -0,0 +1,13 @@
+# Test cases for differnetial.jl
+
+println("-----------------------------------------------------------------")
+facts("partial derivatives") do
+    context("") do
+    #=
+    0:3 by themselves
+    negative based on metric
+    Dμ and del_i being built correctly
+    changing div by/into ?
+    =#
+    end
+end
